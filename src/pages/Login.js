62f4057e104debef777960e6b0dbd0f62a3b727a
lucaslangsch <lucas.langsch@gmail.com>
2023-04-11 18:30:41 -0300

    adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { addUser } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      const minLenghtPassword = 6;
+      const emailValidation = email.includes('.com') && email.includes('@');
+      if (password.length >= minLenghtPassword && emailValidation) {
+        this.setState({
+          isButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisabled: true,
+        });
+      }
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(addUser(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isButtonDisabled } = this.state;
+    return (
+      <form>
+        <label>
+          Email:
+          <input
+            type="email"
+            data-testid="email-input"
+            placeholder="Digite aqui seu email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label>
+          Senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            placeholder="Digite aqui sua senha"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={ isButtonDisabled }
+          onClick={ (event) => this.handleSubmit(event) }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        TrybeWallet
+        { email }
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  ...state.user,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
